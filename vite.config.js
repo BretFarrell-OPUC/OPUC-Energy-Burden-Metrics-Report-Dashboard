316c214ade63d26c2b0fae60cc734d831cea66dc
BretFarrell-OPUC <Bret.Farrell@puc.oregon.gov>
2026-02-09 11:19:27 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/oregon-energy-burden-dashboard/',
+  base: '/OPUC-Energy-Burden-Metrics-Report-Dashboard/',
   build: {
     outDir: 'dist',
     sourcemap: false
